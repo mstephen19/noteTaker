f12fc201dc2f610127a99da907e5b9983ccfa116
Matt <matthiasvstephens@gmail.com>
2021-10-19 16:21:05 +0200

    GET request working, require and install uuid, prepare to set up post request

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const uuid = require('uuid');
+const db = require('./db/db.json');
+const fs = require('fs');
 
 const app = express();
-const PORT = 8080;
+const PORT = 5000;
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
+app.get('/api/notes', (req, res) => res.json(db));
 
-app.listen(PORT, _=>{
-  console.log(`Serving static asset routes on port ${PORT}!`)
+app.post('api/notes', (req, res) => {
+  res.json(`${req.method} request received!`);
+  
 })
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.listen(PORT, () =>
+  console.log(`Serving static asset routes on port ${PORT}!`)
+);
