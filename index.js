833b433f49c85e8c4b53f8acbe751b58d6136c20
Matt <matthiasvstephens@gmail.com>
2021-10-19 23:41:33 +0200

    set up port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 let db = require('./db/db.json');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
