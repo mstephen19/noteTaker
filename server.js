587aee207f2cf2312d64a73f3d51bb9b403163b5
Matt <matthiasvstephens@gmail.com>
2021-10-19 16:47:00 +0200

    set up POST request

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const uuid = require('uuid');
-const db = require('./db/db.json');
 const fs = require('fs');
+const db = require('./db/db.json');
 
 const app = express();
 const PORT = 5000;
@@ -16,9 +16,12 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes
 
 app.get('/api/notes', (req, res) => res.json(db));
 
-app.post('api/notes', (req, res) => {
-  res.json(`${req.method} request received!`);
-  
+app.post('/api/notes', (req, res) => {
+  res.status(200).send(`Received ${req.method} request!`)
+  // let toWrite = db.push(req.body)
+  db.push(req.body)
+  fs.writeFile('./db/db.json', JSON.stringify(db, null, '\t'), err => err ? console.error(err) : null)
+
 })
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
